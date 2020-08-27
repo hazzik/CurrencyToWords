e5da3e76082a8ffa66a4e1224a9954d4ef7ed564
Alex Zaytsev <hazzik@gmail.com>
2020-08-27 16:20:58 +1200

    Implement some whole dollar amounts

---
+++ src/CurrencyToWords.Tests/CurrencyToWordTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CurrencyToWords.Tests
+{
+    [TestFixture]
+    public class CurrencyToWordTests
+    {
+        [TestCase(2, "two dollars")]
+        [TestCase(123, "one hundred and twenty three dollars")]
+        public void TestWholeAmounts(decimal amount, string expected)
+        {
+            var result = amount.CurrencyToWords();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ src/CurrencyToWords/CurrencyToWordExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyToWords
+{
+    public static class CurrencyToWordExtensions
+    {
+        public static string CurrencyToWords(this decimal amount)
+        {
+            var dollars = (long) amount;
+            return dollars.ToWords() + " dollars";
+        }
+    }
+}
