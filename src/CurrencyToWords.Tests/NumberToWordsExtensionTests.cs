4ed80df339b03a4d45f814c314e894440e2e9c9b
Alex Zaytsev <hazzik@gmail.com>
2020-08-27 15:27:59 +1200

    Implement number below twenty as they do not have a pattern

---
+++ src/CurrencyToWords.Tests/NumberToWordsExtensionTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace CurrencyToWords.Tests
+{
+    [TestFixture]
+    public class NumberToWordsExtensionTests
+    {
+        [TestCase(0, "zero")]
+        [TestCase(1, "one")]
+        [TestCase(2, "two")]
+        [TestCase(3, "three")]
+        [TestCase(4, "four")]
+        [TestCase(5, "five")]
+        [TestCase(6, "six")]
+        [TestCase(7, "seven")]
+        [TestCase(8, "eight")]
+        [TestCase(9, "nine")]
+        [TestCase(10, "ten")]
+        [TestCase(11, "eleven")]
+        [TestCase(12, "twelve")]
+        [TestCase(13, "thirteen")]
+        [TestCase(14, "fourteen")]
+        [TestCase(15, "fifteen")]
+        [TestCase(16, "sixteen")]
+        [TestCase(17, "seventeen")]
+        [TestCase(18, "eighteen")]
+        [TestCase(19, "nineteen")]
+        public void NumberToWordsUnderTwenty(long number, string expected)
+        {
+            var result = number.ToWords();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ src/CurrencyToWords/NumberToWordExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CurrencyToWords
+{
+    public static class NumberToWordExtensions
+    {
+        private static string[] _units = 
+        {
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+        };
+
+        public static string ToWords(this long number)
+        {
+            if (number < 20)
+                return _units[number];
+
+            throw new NotImplementedException();
+        }
+    }
+}
