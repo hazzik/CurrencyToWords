8fecd3b55b0f8904f207d1efb3cfda7efc225461
Alex Zaytsev <hazzik@gmail.com>
2020-08-27 16:25:21 +1200

    Implement dollar inflections

--- src/CurrencyToWords.Tests/CurrencyToWordTests.cs
+++ src/CurrencyToWords.Tests/CurrencyToWordTests.cs
@@ -5,9 +5,22 @@ namespace CurrencyToWords.Tests
     [TestFixture]
     public class CurrencyToWordTests
     {
+        [TestCase(0, "zero dollars")]
         [TestCase(2, "two dollars")]
         [TestCase(123, "one hundred and twenty three dollars")]
         public void TestWholeAmounts(decimal amount, string expected)
+        {
+            var result = amount.CurrencyToWords();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(1, "one dollar")]
+        [TestCase(11, "eleven dollars")]
+        [TestCase(21, "twenty one dollar")]
+        [TestCase(71, "seventy one dollar")]
+        [TestCase(111, "one hundred and eleven dollars")]
+        [TestCase(151, "one hundred and fifty one dollar")]
+        public void AmountsEndingWithOne(decimal amount, string expected)
         {
             var result = amount.CurrencyToWords();
             Assert.That(result, Is.EqualTo(expected));
--- src/CurrencyToWords/CurrencyToWordExtensions.cs
+++ src/CurrencyToWords/CurrencyToWordExtensions.cs
@@ -5,7 +5,13 @@
         public static string CurrencyToWords(this decimal amount)
         {
             var dollars = (long) amount;
-            return dollars.ToWords() + " dollars";
+            return dollars.ToWords() + " dollar" + Inflection(dollars);
+        }
+
+        private static string Inflection(long amount)
+        {
+            if (amount % 10 == 1 && amount % 100 != 11) return string.Empty;
+            return "s";
         }
     }
 }
