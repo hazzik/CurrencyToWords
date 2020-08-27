027c536843bf71b4b214c163bf255972816a88d4
Alex Zaytsev <hazzik@gmail.com>
2020-08-27 16:48:11 +1200

    Implement cents and fractional numbers

--- src/CurrencyToWords.Tests/CurrencyToWordTests.cs
+++ src/CurrencyToWords.Tests/CurrencyToWordTests.cs
@@ -8,7 +8,7 @@ namespace CurrencyToWords.Tests
         [TestCase(0, "zero dollars")]
         [TestCase(2, "two dollars")]
         [TestCase(123, "one hundred and twenty three dollars")]
-        public void TestWholeAmounts(decimal amount, string expected)
+        public void WholeAmounts(decimal amount, string expected)
         {
             var result = amount.CurrencyToWords();
             Assert.That(result, Is.EqualTo(expected));
@@ -21,6 +21,35 @@ namespace CurrencyToWords.Tests
         [TestCase(111, "one hundred and eleven dollars")]
         [TestCase(151, "one hundred and fifty one dollar")]
         public void AmountsEndingWithOne(decimal amount, string expected)
+        {
+            var result = amount.CurrencyToWords();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(0.02, "two cents")]
+        [TestCase(0.12, "twelve cents")]
+        [TestCase(0.50, "fifty cents")]
+        public void Cents(decimal amount, string expected)
+        {
+            var result = amount.CurrencyToWords();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(0.01, "one cent")]
+        [TestCase(0.11, "eleven cents")]
+        [TestCase(0.21, "twenty one cent")]
+        [TestCase(0.71, "seventy one cent")]
+        public void CentsEndingWithOne(decimal amount, string expected)
+        {
+            var result = amount.CurrencyToWords();
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(0, "zero dollars")]
+        [TestCase(0.12, "twelve cents")]
+        [TestCase(10.55, "ten dollars and fifty five cents")]
+        [TestCase(120, "one hundred and twenty dollars")]
+        public void RequiredCases(decimal amount, string expected)
         {
             var result = amount.CurrencyToWords();
             Assert.That(result, Is.EqualTo(expected));
--- src/CurrencyToWords/CurrencyToWordExtensions.cs
+++ src/CurrencyToWords/CurrencyToWordExtensions.cs
@@ -1,11 +1,38 @@
-﻿namespace CurrencyToWords
+﻿using System.Collections.Generic;
+
+namespace CurrencyToWords
 {
     public static class CurrencyToWordExtensions
     {
         public static string CurrencyToWords(this decimal amount)
         {
-            var dollars = (long) amount;
-            return dollars.ToWords() + " dollar" + Inflection(dollars);
+            if (amount == 0)
+            {
+                return "zero dollars";
+            }
+
+            var dollars = GetWholeDollars(amount);
+            var cents = GetOnlyCents(amount);
+
+            var parts = new List<string>();
+            if (dollars > 0)
+            {
+                parts.Add($"{dollars.ToWords()} dollar{Inflection(dollars)}");
+            }
+
+            if (cents > 0)
+            {
+                parts.Add($"{cents.ToWords()} cent{Inflection(cents)}");
+            }
+
+            return string.Join(" and ", parts);
+        }
+  
+        private static long GetWholeDollars(decimal amount) => (long) amount;
+
+        private static long GetOnlyCents(decimal amount)
+        {
+            return (long) (amount * 100 % 100);
         }
 
         private static string Inflection(long amount)
